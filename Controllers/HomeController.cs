9b361605b52a38f043c89782b8ac9a75da64f6c5
Mounika <mounika.tunga9@gmail.com>
2017-08-17 10:52:01 -0700

    login and reg

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,16 +18,60 @@ namespace dojostore.Controllers
         }
         // GET: /Home/
         [HttpGet]
-        [Route("Customers")]
-        public IActionResult Customers()
+        [Route("")]
+        public IActionResult Index()
         {
-            ViewBag.users = _context.users.ToList();
             return View();
         }
         [HttpPost]
-        [Route("add")]
-        public IActionResult add(User NewUser){
-            
+        [Route("register")]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid){
+                User NewUser = new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailId = model.EmailId,
+                    Password = model.Password,
+                   
+                };
+                _context.Add(NewUser);
+                _context.SaveChanges();
+                System.Console.WriteLine("Done adding to DB");
+                User EnteredPerson = _context.users.SingleOrDefault(user => user.EmailId == model.EmailId);
+                HttpContext.Session.SetString("FirstName", EnteredPerson.FirstName);
+                HttpContext.Session.SetInt32("UserId",EnteredPerson.UserId);
+                return RedirectToAction("customers");
+            }
+            else 
+            {
+                ViewBag.errors = ModelState.Values;
+                 ViewBag.Errors = new List<string>();
+                return View("Index");
+            }
         }
+        [HttpPost]
+        [Route("PostLogin")]
+        public IActionResult PostLogin(string EmailId, string Password)
+        {
+             User loggedUser = _context.users.SingleOrDefault(user => user.EmailId == EmailId);
+             if (loggedUser != null)
+             {
+                HttpContext.Session.SetString("FirstName", loggedUser.FirstName);
+                HttpContext.Session.SetInt32("UserId", loggedUser.UserId);
+                return RedirectToAction("customers");
+             }
+
+               ViewBag.Errors = new List<string>(){ "Check Username and password"};
+                return View("Index");
+        }
+        [HttpGet]
+        [Route("logout")]
+        public IActionResult Logout(){
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+       
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,8 @@ namespace dojostore
     public class User : BaseEntity
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string EmailId { get; set; }
         public string Password { get; set; }
         public List<Order> orders { get; set; }
